216fd24d4abd9b5dfdd8b58aa3164056ae70b515
Daniel Hammerl <daniel.hammerl99@gmx.de>
2021-11-15 19:25:37 +0100

    register endpoint should only be called with password

--- src/controller/RegistryController.ts
+++ src/controller/RegistryController.ts
@@ -1,27 +1,35 @@
 import express from 'express';
 import { addService, RegisteredServiceShape } from '../util/registeredServices';
-import { log, ValidationException } from '@danielhammerl/nodejs-service-framework';
+import { getConfig, HttpException, log, ValidationException } from '@danielhammerl/nodejs-service-framework';
 import { saveRegisteredServices } from '../util/database';
 
 const router = express.Router();
 
 router.post('', (req, res) => {
   log('info', 'Some service tries to register...');
+
   try {
     RegisteredServiceShape.validateSync(req.body);
   } catch (e) {
     throw new ValidationException(e as Error);
   }
-  log('info', `New service registered: ${req.body.applicationName}`, {
-    metadata: {
-      serviceData: req.body,
-    },
-  });
 
-  addService(req.body.applicationName, req.body.port);
-  saveRegisteredServices();
+  if (req.body.password !== getConfig('serviceRegistryPassphrase')) {
+    setTimeout(() => {
+      return res.status(403).send();
+    }, 5000);
+  } else {
+    log('info', `New service registered: ${req.body.applicationName}`, {
+      metadata: {
+        serviceData: req.body,
+      },
+    });
 
-  return res.json({ connected: true }).status(200);
+    addService(req.body.applicationName, req.body.port);
+    saveRegisteredServices();
+
+    return res.json({ connected: true }).status(200);
+  }
 });
 
 export default router;
